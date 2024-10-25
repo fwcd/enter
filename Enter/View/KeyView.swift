7dabc46217f0d267fe8c54db3f0415dd94274d58
fwcd <fwcd@fwcd.dev>
2024-10-25 14:31:35 +0200

    Format key switch (ha!) slightly more concisely

--- Enter/View/KeyView.swift
+++ Enter/View/KeyView.swift
@@ -17,58 +17,35 @@ struct KeyView: View {
             .aspectRatio(1, contentMode: .fit)
             .overlay {
                 switch key {
-                case .delete:
-                    Image(systemName: "delete.left")
-                case .deleteForward:
-                    Image(systemName: "delete.right")
-                case .return:
-                    Image(systemName: "return")
-                case .space:
-                    Image(systemName: "space")
-                case .tab:
-                    Image(systemName: "arrow.right.to.line")
-                case .escape:
-                    Image(systemName: "escape")
-                case .clear:
-                    Image(systemName: "trash")
-                case .home:
-                    Image(systemName: "arrow.up.to.line")
-                case .end:
-                    Image(systemName: "arrow.down.to.line")
-                case .pageUp:
-                    Image(systemName: "arrow.up")
-                case .pageDown:
-                    Image(systemName: "arrow.down")
+                case .delete: Image(systemName: "delete.left")
+                case .deleteForward: Image(systemName: "delete.right")
+                case .return: Image(systemName: "return")
+                case .space: Image(systemName: "space")
+                case .tab: Image(systemName: "arrow.right.to.line")
+                case .escape: Image(systemName: "escape")
+                case .clear: Image(systemName: "trash")
+                case .home: Image(systemName: "arrow.up.to.line")
+                case .end: Image(systemName: "arrow.down.to.line")
+                case .pageUp: Image(systemName: "arrow.up")
+                case .pageDown: Image(systemName: "arrow.down")
                 case .modifier(let modifier):
                     switch modifier {
-                    case .capsLock:
-                        Image(systemName: "capslock")
-                    case .control:
-                        Image(systemName: "control")
-                    case .option:
-                        Image(systemName: "option")
-                    case .shift:
-                        Image(systemName: "shift")
-                    case .command:
-                        Image(systemName: "command")
-                    case .function:
-                        Image(systemName: "globe")
+                    case .capsLock: Image(systemName: "capslock")
+                    case .control: Image(systemName: "control")
+                    case .option: Image(systemName: "option")
+                    case .shift: Image(systemName: "shift")
+                    case .command: Image(systemName: "command")
+                    case .function: Image(systemName: "globe")
                     }
                 case .arrow(let direction):
                     switch direction {
-                    case .left:
-                        Image(systemName: "arrowtriangle.left")
-                    case .right:
-                        Image(systemName: "arrowtriangle.right")
-                    case .up:
-                        Image(systemName: "arrowtriangle.up")
-                    case .down:
-                        Image(systemName: "arrowtriangle.down")
+                    case .left: Image(systemName: "arrowtriangle.left")
+                    case .right: Image(systemName: "arrowtriangle.right")
+                    case .up: Image(systemName: "arrowtriangle.up")
+                    case .down: Image(systemName: "arrowtriangle.down")
                     }
-                case .function(let n):
-                    Text("F\(n)")
-                case .character(let c):
-                    Text("\(c)")
+                case .function(let n): Text("F\(n)")
+                case .character(let c): Text("\(c)")
                 }
             }
             .font(.system(size: size))
